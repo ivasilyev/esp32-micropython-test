e0d5df8c9cdf40f515f84d4c2ed7d71f60315f3b
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2020-12-07 21:20:36 +0300

    Tested async in single-timers

--- main.test.py
+++ main.test.py
@@ -1,3 +1,4 @@
+import uasyncio as asyncio
 from colors import BoilerPlate as Color
 from demo import Strip, Animations, ColorManager
 
@@ -5,16 +6,22 @@ strip = Strip(17, 16, brightness=1, auto_write=True)
 animations = Animations(strip)
 # animations.animate(animations.cycle2, colors=ColorManager.create_color_loop([Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Aqua, Color.Blue, Color.Purple], 8), pause=10, always_lit=True)
 
-import uasyncio as asyncio
 
 
 async def coro_function1():
     animations.cycle2(colors=ColorManager.create_color_loop([Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Aqua, Color.Blue, Color.Purple], 8), pause=10, always_lit=True)
     return 1
 
+
 async def coro_function2():
     animations.cycle2(colors=ColorManager.create_color_loop([Color.Red, Color.Aqua], 8), pause=10, always_lit=False)
     return 2
 
+
 async def get():
     return await coro_function1() + await coro_function2()
+
+loop = asyncio.get_event_loop()
+a = loop.create_task(get())
+# loop.run_until_complete(a)
+loop.run_forever()
