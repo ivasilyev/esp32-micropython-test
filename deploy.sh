ec9c0ce6b8adbfd3445ef385d326d725714991b9
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2020-12-07 11:01:10 +0300

    Added boot file

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-PORT=COM5
-
+# ampy --port COM5 --baud 115200 get "boot.py" "boot.py"
 for FILE in __init__.py main.py config.json utils.py demo.py http_server.py index.html styles.css color_maps.py colors.py
   do
     echo Copy ${FILE}
-    ampy --port ${PORT} --baud 115200 put ${FILE}
+    ampy put ${FILE}
   done
