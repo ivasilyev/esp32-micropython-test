fdd03c251ed774e50cfcb67ab6bab8b8d0cb6e1c
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2020-12-08 18:46:59 +0300

    Added custom hostname to the NIC setup

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from utils import Utils
 
-cfg_dict = Utils.read_cfg("config.json")
-Utils.do_connect(cfg_dict["network"]["wlan"]["ssid"], cfg_dict["network"]["wlan"]["password"])
-
-
+cfg_dict = Utils.read_json("config.json")
+Utils.wlan_connect(wlan_essid=cfg_dict["network"]["wlan"]["ssid"],
+                   wlan_password=cfg_dict["network"]["wlan"]["password"],
+                   hostname="esp32")
--- utils.py
+++ utils.py
@@ -11,37 +11,40 @@ class Utils:
         return s
 
     @staticmethod
-    def read_cfg(cfg):
+    def read_json(file: str):
         from json import load
-        with open(file=cfg, mode="r", encoding="utf-8") as f:
+        with open(file=file, mode="r", encoding="utf-8") as f:
             out = load(f)
             f.close()
         return out
 
     @staticmethod
-    def do_connect(wlan_essid, wlan_password):
+    def wlan_connect(wlan_essid, wlan_password, hostname: str = ""):
         from network import WLAN, STA_IF
-        sta_if = WLAN(STA_IF)
-        if not sta_if.isconnected():
+        from utime import sleep
+        controller = WLAN(STA_IF)
+        if not controller.isconnected():
             print("Connecting to network: ...")
-            sta_if.active(True)
-            sta_if.connect(wlan_essid, wlan_password)
-            while not sta_if.isconnected():
-                pass
-        print("Network connected, the config: {}".format(sta_if.ifconfig()))
+            controller.active(True)
+            if len(hostname) > 0:
+                controller.config(dhcp_hostname=hostname)
+            controller.connect(wlan_essid, wlan_password)
+            while not controller.isconnected():
+                sleep(1)
+        print("Network connected, the config: {}".format(controller.ifconfig()))
 
     @staticmethod
     def rm(s):
-        import uos
+        import uos as os
         try:
-            _ = uos.stat(s)[0]
+            _ = os.stat(s)[0]
             try:
-                uos.remove(s)  # A file
+                os.remove(s)  # A file
             except OSError:  # A folder
-                for i in uos.listdir(s):
+                for i in os.listdir(s):
                     s_ = "{}/{}".format(s, i)
                     Utils.rm(s_)
-                uos.rmdir(s)
+                os.rmdir(s)
         except OSError:
             print("Not found: '{}'".format(s))
 
@@ -55,11 +58,11 @@ class Utils:
 
     @staticmethod
     def timeit(func, run_number: int = 1, *args, **kwargs):
-        import utime
-        start = utime.ticks_us()
+        import utime as time
+        start = time.ticks_us()
         for i in range(run_number):
             _ = func(*args, **kwargs)
-        return utime.ticks_diff(utime.ticks_us(), start)
+        return time.ticks_diff(time.ticks_us(), start)
 
     @staticmethod
     def soft_reset():
